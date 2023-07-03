252821d28906a13e8527a93f68e553098c7977a2
napul205 <100463532+napul205@users.noreply.github.com>
2023-07-03 09:45:31 +0530

    Added solution - LeetHub

---
+++ Maximum Index - GFG/maximum-index.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int maxIndexDiff(int arr[], int n) {
+        // code here
+        int i=0,j=n-1;
+        int di=0;
+        int m=0;
+        
+        while(i<j)
+        {
+            if(arr[j]>=arr[i])
+            {
+                di=j-i;
+                m=max(m,di);
+                i++;
+                j=n-1;
+            }
+            else
+            j--;
+            
+        }
+        return m;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.maxIndexDiff(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
