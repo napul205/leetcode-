b74e4951c60448583175205bf81b00efdb0ac989
napul205 <100463532+napul205@users.noreply.github.com>
2023-08-08 17:38:32 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (92.75%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        
         vector<int> ans;
         
-        int top=0;
-        int bottom=matrix.size()-1;
-        int left=0;
-        int right=matrix[0].size()-1;
-        
-        
-        while(top<=bottom && left<=right)
+        int nr=matrix.size();
+        int nc=matrix[0].size();
+        int dir=0;
+        int t=0,b=nr-1,l=0,r=nc-1;
+        while(t<=b && l<=r)
         {
-           for(int i=left;i<=right;i++)
-               ans.push_back(matrix[top][i]);
-            
-            top++;
-            
-            for( int i=top;i<=bottom;i++ )
-                ans.push_back(matrix[i][right]);
-            
-            right--;
-            
-            if(top<=bottom)
+            if(dir==0)
             {
-                for(int i=right;i>=left;i--)
-                    ans.push_back(matrix[bottom][i]);
-                
-                bottom--;
+                for( int i=l;i<=r;i++)
+                    ans.push_back(matrix[t][i]);
+                t++;
+                dir=1;
             }
-            
-            if(left<=right)
-            {
-                for(int i=bottom;i>=top;i--)
-                    ans.push_back(matrix[i][left]);
-                left++;
-            }
+            else
+                if(dir==1)
+                {
+                    for( int i=t;i<=b;i++)
+                        ans.push_back(matrix[i][r]);
+                    r--;
+                    dir=2;
+                }else
+                    if(dir==2)
+                    {
+                        for( int i=r;i>=l;i--)
+                            ans.push_back(matrix[b][i]);
+                        dir=3;
+                        b--;
+                        
+                    }else
+                        if(dir=3)
+                        {
+                            for( int i=b;i>=t;i--)
+                                ans.push_back(matrix[i][l]);
+                            l++;
+                            dir=0;
+                        }
+                        
         }
         
         return ans;
