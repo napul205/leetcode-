85d65a90f62ed7fae638a212e012317b86115018
napul205 <100463532+napul205@users.noreply.github.com>
2023-09-19 17:17:01 +0530

    Time: 80 ms (74.03%), Space: 61.6 MB (26.75%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
      bool vis[100005]={0};
-        // ios::sync_with_stdio(0);
-        // cin.tie(0);
-        // cout.tie(0);
+       
         for(int i=0;i<nums.size();i++){
             if(vis[nums[i]]){
                 nums.clear();
