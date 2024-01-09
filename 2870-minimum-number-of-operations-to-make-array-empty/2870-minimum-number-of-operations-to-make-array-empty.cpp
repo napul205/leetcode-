ed1bcb724cceaf0f0500cab4f7b5503cc71bb720
napul205 <100463532+napul205@users.noreply.github.com>
2024-01-09 11:30:48 +0530

    Time: 138 ms (13.93%), Space: 92 MB (5.12%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+      map<int,int> mp;
+        
+        for( auto it:nums)
+        {
+            mp[it]++;
+        }
+        int ans=0;
+        
+        for( auto it: mp)
+        {
+            if( it.second==1)
+                return -1;
+            
+            
+            ans+=ceil((double)it.second/3);
+        }
+        
+        return ans;
+    }
+};
